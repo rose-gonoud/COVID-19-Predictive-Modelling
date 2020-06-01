13dbdf8d35a61d8899a86e1f145b73b152d842d3
ShaunR <shaunrmrz742@gmail.com>
2020-05-31 17:21:47 -0700

    Add Covid Data Bar Graph

--- assets/js/barGraphEmpl.js
+++ assets/js/barGraphEmpl.js
@@ -1,4 +1,4 @@
-function buildBarGraph(graphData){
+function buildEmplBarGraph(graphData){
 
 
   summedCountyData = {};
@@ -26,9 +26,9 @@ function buildBarGraph(graphData){
 
 
   //
-  console.log("######## Summed unemployment DATA ############");
+  //console.log("######## Summed unemployment DATA ############");
   //console.log(summed_unemployment_data);
-  console.log(summedCountyData);
+  //console.log(summedCountyData);
 
   //list of State abbreviations to iterate through and grab required plot info from state_unemployment_data object
   var stateAbbrList = Object.keys(summedCountyData);
@@ -63,10 +63,8 @@ function buildBarGraph(graphData){
 
   });
 
-
-  console.log("****data input list****")
+  //console.log("****data input list****")
   //console.log(data_input);
-
 
   var data = data_input;
   //console.log(data);
@@ -74,4 +72,75 @@ function buildBarGraph(graphData){
   var layout = {barmode: 'stack', showlegend:false, xaxis:{tickangle: -45},hovermode:'closest'};
 
   Plotly.newPlot(id='graph', data, layout);
+}
+
+//County level Covid-19 BarGraph
+function covidBarGraph(covData){
+
+  covidCountyData = {};
+  // console.log("******Covid Data*******");
+  // console.log(covData);
+  //
+  covData.forEach((state) => {
+
+    if(!(state.region.province in covidCountyData)){ //start building summedCountyData object by checking if item.stat_abbr exists in it
+      covidCountyData[state.region.province] = [];
+    }
+
+    state.region.cities.forEach((city_name) => {
+      if(!(city_name.name in covidCountyData)){//item.name is not defined in state object, so item.county_name must be
+        covidCountyData[state.region.province].push({"name" : city_name.name, "confirmed" : city_name.confirmed});
+      }
+    });
+
+
+  });
+
+
+  //
+  // console.log("######## Covid DATA ############");
+  // console.log(covidCountyData);
+
+  //list of State to iterate through and grab required plot info from state_unemployment_data object
+  var stateList = Object.keys(covidCountyData);
+  //console.log(stateAbbrList);
+  //var unemployment_rate = Object.values(summed_unemployment_data);
+
+  let data_input = [];
+
+  stateList.forEach((state) => {
+    // if(state === "AK"){
+    covidCountyData[state].forEach((city) => {
+      var randomColor = Math.floor(Math.random()*16777215).toString(16);
+
+      let trace2 =
+        {
+          x: [state],
+          y: [city.confirmed],
+          name: city.name,
+          marker: {color:"#"+randomColor},
+          hoverinfo: "name",
+          type: 'bar'
+        };
+      //console.log(trace_name);
+      data_input.push(trace2);
+
+
+    });
+
+  //}
+
+  });
+  //
+  //
+  // console.log("****data input list****")
+  // console.log(data_input);
+  //
+  //
+  var data = data_input;
+  //console.log(data);
+
+  var layout = {barmode: 'stack', showlegend:false, xaxis:{tickangle: -45}, hovermode:'closest'};
+
+  Plotly.newPlot(id='plot', data, layout);
 }
